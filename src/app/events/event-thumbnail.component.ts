74e5aa3bf0e33182aea886e95da1794af898955a
Brandon <thebombdad32@gmail.com>
2023-05-29 22:21:40 -0500

    Added Navbar

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: 'event-thumbnail',
@@ -10,13 +10,16 @@ import { Component, Input } from "@angular/core";
     <div>Price: \${{event.price}}</div>
     <div>
       <span>Location: {{event.location.address}}</span>
-      <span>&nbsp;</span>
-      <span>{{event.location.city}}, {{event.location.country}}</span>
+      <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
     </div>
   </div>
-  `
+  `,
+  styles: [`
+    .pad-left { margin-left: 10px; }
+    `
+  ]
 })
 
 export class EventThumbnailComponent {
-  @Input() event:any
-}
+  @Input() event:any;
+  }
